c5ae37794cb9f7738f990abf6290fdf41a2558a9
SheldonWang <naozhong@gmail.com>
2023-01-03 22:26:07 -0500

    fix lint

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React from 'react'
-import './App.css'
+import "./App.css";
 
-function App () {
-  return <div className="App">Welcome to SpeakToGPT</div>
+function App() {
+  return <div className="App">Welcome to SpeakToGPT</div>;
 }
 
-export default App
+export default App;
