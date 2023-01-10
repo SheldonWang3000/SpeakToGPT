8815a9688ba3260d68cae93255b2305b2bc8c4cf
SheldonWang <naozhong@gmail.com>
2023-01-10 15:02:25 -0500

    first test on speech to text api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import getTokenOrRefresh from "./getTokenOrRefresh";
+import { ResultReason } from "microsoft-cognitiveservices-speech-sdk";
+
+const speechsdk = require("microsoft-cognitiveservices-speech-sdk");
 
 function App() {
-  const [authObj, setAuthObj] = useState("");
+  const [authObj, setAuthObj] = useState(null);
+  const [voiceText, setVoiceText] = useState("");
   useEffect(() => {
     const fetch = async () => {
       const auth = await getTokenOrRefresh();
@@ -11,7 +15,38 @@ function App() {
     };
     fetch();
   }, []);
-  return <div className="App">Welcome to SpeakToGPT: {authObj.authToken}</div>;
+  const startVoice = () => {
+    if (authObj !== null) {
+      const speechConfig = speechsdk.SpeechConfig.fromAuthorizationToken(
+        authObj.authToken,
+        authObj.region
+      );
+      speechConfig.speechRecognitionLanguage = "en-US";
+
+      const audioConfig = speechsdk.AudioConfig.fromDefaultMicrophoneInput();
+      const recognizer = new speechsdk.SpeechRecognizer(
+        speechConfig,
+        audioConfig
+      );
+      console.log(recognizer);
+      recognizer.recognizeOnceAsync((result) => {
+        let displayText;
+        if (result.reason === ResultReason.RecognizedSpeech) {
+          displayText = `Me: ${result.text}`;
+        } else {
+          displayText =
+            "ERROR: Speech was cancelled or could not be recognized. Ensure your microphone is working properly.";
+        }
+        setVoiceText(displayText);
+      });
+    }
+  };
+  return (
+    <div>
+      <button onClick={() => startVoice()}>start</button>
+      <div className="App">{voiceText}</div>
+    </div>
+  );
 }
 
 export default App;
